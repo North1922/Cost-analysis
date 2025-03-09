dd3d9cf318d5ca7d06118bd65c370625b84acd21
Alexander Sevryukov <koshmarvrn@gmail.com>
2025-03-09 18:42:48 +0300

    fix buf and add readme

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import datetime
+import colorama
+
+from colorama import Fore, Back, Style
 from datetime import date
 
-
+colorama.init()# инициализируем colorama перед использованием
 records = [] #список для зранения записей представленных кортежами (название покупки , сумма, категория)
 shopping_dict = {} #словарь ключ-категория: значение-список кортежей(название покупки, сумма)
 the_amount = 0 # сумма расходов
 average_expense = 0# средние расходы
 while True:
-    purchases = input('Введите покупки в формате: название покупки:сумма:категория. '
-                      '"Стоп" для завершения ввода.').lower()
+    purchases = input(Fore.GREEN + 'Введите покупки в формате:'+ Style.RESET_ALL + ' название покупки:сумма:категория. ' +
+                      Fore.BLUE + Style.BRIGHT +'Стоп'+ Style.RESET_ALL + ' для завершения ввода.').lower()
     if purchases == 'стоп':
         break
 
     parts = purchases.split(':')
 
     if len(parts) != 3 or not purchases.split(':')[1].isdigit():
-        print('Некорректный формат ввода')
+        print(Fore.RED + Style.BRIGHT + 'Некорректный формат ввода' + Style.RESET_ALL)
         continue
 
     records.append(tuple(parts))# преобразовали полученную строку parts в кортеж и добавили в список records, что бы
@@ -33,10 +36,11 @@ while True:
 
 
 average_expense = the_amount / len(records)
-print('--------------------------')
-print(f'Кол-во покупок: {len(records)}. Общая сумма расходов: {the_amount}. '
-      f'Среднее значение расходов по всем категориям: {average_expense}')
-print('--------------------------')
+print(Fore.RED + '--------------------------' + Style.RESET_ALL)
+print(Fore.CYAN + f' Кол-во покупок:{Style.RESET_ALL} {len(records)} ||' +
+      Fore.CYAN + f' Общая сумма расходов:{Style.RESET_ALL} {the_amount} ||' +
+      Fore.CYAN + f' Среднее значение расходов по всем категориям:{Style.RESET_ALL} {int(average_expense)} ||')
+print(Fore.RED + '--------------------------' + Style.RESET_ALL)
 list_of_keys = list(shopping_dict.keys()) # получили список с ключами
 index = 0 # счётчик, который будем использовать для доступа к элементам списка
 # Используем цикл while для итерации по категориям
@@ -55,11 +59,11 @@ while index < len(list_of_keys):
             purchases_str += ", "
         i += 1
     # Выводим результаты
-    print(f"Категория: {category}")
-    print(f"Список покупок: {purchases_str}")
-    print(f"Суммарная стоимость: {total_amount}")
-    print(f"Средняя сумма: {average_amount}")
-    print('--------------------------')  # Пустая строка для разделения категорий
+    print(Fore.YELLOW + "Категория покупок: " + Style.RESET_ALL + category)
+    print(Fore.YELLOW + "Список покупок: " + Style.RESET_ALL + purchases_str)
+    print(Fore.YELLOW + "Суммарная стоимость: " + Style.RESET_ALL + str(total_amount))
+    print(Fore.YELLOW + "Средняя сумма: " + Style.RESET_ALL + str(average_amount))
+    print(Fore.RED + '--------------------------' + Style.RESET_ALL)  # Пустая строка для разделения категорий
 
     # Увеличиваем индекс для перехода к следующей категории
     index += 1
@@ -69,4 +73,4 @@ time_now = datetime.datetime.now() #для получения времени, с
 # а затем достать из этого объекта только время с помощью метода time():
 current_time = time_now.time().strftime('%X') #достаём из time_now время при помощи .time() и форматируем при помощи
 # strftime('%X') в локальную версию времени
-print(f"Дата: {date_now}. Время: {current_time}.")
+print(f"{Fore.MAGENTA}Дата:{Style.RESET_ALL} {date_now}. {Fore.MAGENTA}Время:{Style.RESET_ALL} {current_time}.")
